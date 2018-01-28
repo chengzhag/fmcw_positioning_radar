b74d448d0960e4ce732e6e1356d258017ce010c6
Unknown <850576570@qq.com>
2018-01-28 10:47:48 +0800

    编写基于mbed的天线切换程序，相位抖动约0.15us

--- mcu/antenna_switch_mbed/antenna_switch_mbed/main.cpp
+++ mcu/antenna_switch_mbed/antenna_switch_mbed/main.cpp
@@ -1,14 +1,62 @@
 #include <mbed.h>
 
-DigitalOut g_LED(LED1);
+#define WIDTH_BIT 100
+
+InterruptIn triger(D4);
+DigitalOut switchV1(D2), switchV2(D3);
+DigitalOut usrpSyn(D5);
+
+void switchAnt()
+{
+	static int Rxi = 0;
+	usrpSyn=1;
+	switch (Rxi)
+	{
+	case 0:
+		switchV1 = 0;
+		switchV2 = 0;
+		break;
+	case 1:
+		switchV1 = 1;
+		switchV2 = 0;
+		break;
+	case 2:
+		switchV1 = 0;
+		switchV2 = 1;
+		break;
+	}
+	wait_us(WIDTH_BIT);
+	switch (Rxi)
+	{
+	case 0:
+		usrpSyn = 0;
+		wait_us(WIDTH_BIT);
+		usrpSyn = 0;
+		break;
+	case 1:
+		usrpSyn = 1;
+		wait_us(WIDTH_BIT);
+		usrpSyn = 0;
+		break;
+	case 2:
+		usrpSyn = 0;
+		wait_us(WIDTH_BIT);
+		usrpSyn = 1;
+		break;
+	}
+	wait_us(WIDTH_BIT);
+	usrpSyn = 0;
+	
+	if (++Rxi>=3)
+	{
+		Rxi = 0;
+	}
+}
 
 int main() 
 {
+	triger.fall(&switchAnt);
 	for (;;)
 	{
-		g_LED = 1;
-		wait_ms(500);
-		g_LED = 0;
-		wait_ms(500);
 	}
 }
