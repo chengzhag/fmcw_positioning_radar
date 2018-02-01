36e14c297a41ed0313a33f8d397abbfd950c502c
Unknown <850576570@qq.com>
2018-02-01 12:19:05 +0800

    修复在采用一根天线时各simulink模块的错误，修复positioning_test.m绘图时天线数量的错误。暂时改为1根天线进行测试。
    Former-commit-id: 8edd3dc5b39ace58bf7c3e80111ff89d3be7a17d

--- matlab/positioning_test.m
+++ matlab/positioning_test.m
@@ -6,7 +6,7 @@ doFindFirstpeakSampleTest_findpeaks=0;
 close all;
 
 %% 加载/提取数据、参数
-load '../data/foreground_1MHz_400rps_5rpf_1t3r_walking.mat'
+% load '../data/foreground_1MHz_400rps_5rpf_1t3r_walking.mat'
 
 nRx=size(antBits,1);
 fo=log2array(logsout,'foregroundSim');
@@ -50,8 +50,8 @@ ds=ds(valD);
 
 %% 显示三根天线瀑布图
 figure('name','三根天线瀑布图');
-for iRx=1:3
-    subplot(1,3,iRx);
+for iRx=1:nRx
+    subplot(1,nRx,iRx);
     imagesc(ds,ts,log(fo(:,:,iRx)'));
     xlabel('d(m)');
     ylabel('t(s)');
@@ -63,8 +63,8 @@ if doFindpeaksTest_findpeaks
     hFP=figure('name','测试findpeaks函数');
     for iF=1:5:size(fo,2)
         figure(hFP);
-        for iRx=1:3
-            subplot(1,3,iRx);
+        for iRx=1:nRx
+            subplot(1,nRx,iRx);
             findpeaks(fo(:,iF,iRx),ds,'MinPeakProminence',max(fo(:,iF,iRx))*0.8,'Annotate','extents','NPeaks',1);
             title(['第' num2str(ts(iF)) 's Rx' num2str(iRx) '的频谱']);
         end
@@ -77,8 +77,8 @@ if doFindFirstpeakSampleTest_findpeaks
     iSam=find(ts>10,1);
     foSam=permute(fo(:,iSam,:),[1,3,2]);
     hSam=figure('name','示例帧寻峰测试');
-    for iRx=1:3
-        subplot(1,3,iRx);
+    for iRx=1:nRx
+        subplot(1,nRx,iRx);
         plot(ds,foSam(:,iRx));
         xlabel('d(m)');
         title(['第' num2str(ts(iSam)) 's Rx' num2str(iRx) '的频谱']);
@@ -86,8 +86,8 @@ if doFindFirstpeakSampleTest_findpeaks
     
     iFp=findFirstPeak(foSam,0.7);
     figure(hSam);
-    for iRx=1:3
-        subplot(1,3,iRx);
+    for iRx=1:nRx
+        subplot(1,nRx,iRx);
         hold on;
         plot(ds(iFp(iRx)),foSam(iFp(iRx),iRx),'o');
         hold off;
@@ -101,8 +101,8 @@ for iF=1:size(fo,2);
 end
 dsTa=dsTa.*dPs;
 hDT=figure('name','寻峰得到的距离——时间曲线');
-for iRx=1:3
-    subplot(1,3,iRx);
+for iRx=1:nRx
+    subplot(1,nRx,iRx);
     plot(dsTa(:,iRx),ts);
     ylabel('t(s)');
     xlabel('d(m)');
@@ -112,16 +112,16 @@ end
 %% 对距离——时间曲线做异常值剔除和滤波处理
 dsTaHampel=hampel(dsTa,11,0.5);
 figure(hDT);
-for iRx=1:3
-    subplot(1,3,iRx);
+for iRx=1:nRx
+    subplot(1,nRx,iRx);
     hold on;
     plot(dsTaHampel(:,iRx),ts);
 end
 
 dsTaFiltered=filter(0.2,[1,-0.8],dsTaHampel);
 figure(hDT);
-for iRx=1:3
-    subplot(1,3,iRx);
+for iRx=1:nRx
+    subplot(1,nRx,iRx);
     hold on;
     plot(dsTaFiltered(:,iRx),ts);
 end
