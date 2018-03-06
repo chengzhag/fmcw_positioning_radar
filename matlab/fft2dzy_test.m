d0098523e1338a0d37be1a31dbd8dc0b96639424
Unknown <850576570@qq.com>
2018-03-06 22:15:26 +0800

    添加fft2dzy_test.m，用于测试2dfft在z轴方向的简单解算。修复fft2d_test.m的内存预分配问题。

--- matlab/fft2dzy_test.m
+++ matlab/fft2dzy_test.m
@@ -34,23 +34,27 @@ tsRamp=(0:lFft-1)/fS*fftDownFac;
 % yLoCut=yLoCut(:,:,valT);
 % ts=ts(valT);
 
-%% 水平xy方向2DFFT
-heatMaps=fft2(yLoReshape,lFft,nAng);
+%% 竖直zy方向2DFFT
+yLoTsrampTRTs=permute(yLoReshape,[1,3,2,4]);
+heatMaps=fft2(yLoTsrampTRTs,lFft,nAng);
 heatMaps=heatMaps(isD,:,:,:);
 
 heatMaps=circshift(heatMaps,ceil(size(heatMaps,2)/2),2);
-heatMaps=flip(heatMaps,2);
+% heatMaps=flip(heatMaps,2);
 %% 背景消除
 heatMapsB=filter(0.2,[1,-0.8],heatMaps,0,4);
 heatMapsF=abs(heatMaps-heatMapsB);
-heatMapsF=permute(prod(heatMapsF,3),[1,2,4,3]);
+% heatMapsF=permute(prod(heatMapsF,3),[1,2,4,3]);
+% heatMapsF=permute(sum(heatMapsF,3),[1,2,4,3]);
+heatMapsF=permute(prod(heatMapsF(:,:,5:8,:),3),[1,2,4,3]);
 
+%% 显示坐标转换前功率分布
 if doShowHeatMapsBefore
-    %% 显示功率分布
     hHea=figure('name','空间热度图');
     for iFrame=1:size(heatMapsF,3)
         figure(hHea);
         heatMap=heatMapsF(:,:,iFrame);
+        heatMap=heatMap./max(max(heatMap));
         imagesc(angs,dsC,heatMap);
         set(gca, 'XDir','normal', 'YDir','normal');
         title(['第' num2str(ts(iFrame)) 's 的空间热度图']);
@@ -60,13 +64,14 @@ if doShowHeatMapsBefore
     end
 end
 
+%% 显示坐标转换后功率分布
 if doShowHeatMapsAfter
-    %% 极坐标转换
-    xsCoor=single(-8:0.2:8);
+    % 极坐标转换
+    zsCoor=single(-1:0.2:3);
     ysCoor=single(dMi:0.2:dMa);
     
-    [xsMesh,ysMesh]=meshgrid(xsCoor,ysCoor);
-    heatMapsCarF=zeros(length(ysCoor),length(xsCoor),length(ts),'single');
+    [xsMesh,ysMesh]=meshgrid(zsCoor,ysCoor);
+    heatMapsCarF=zeros(length(ysCoor),length(zsCoor),length(ts),'single');
     
     % 计算坐标映射矩阵
     dsPo2Car=sqrt(xsMesh.^2+ysMesh.^2);
@@ -77,17 +82,17 @@ if doShowHeatMapsAfter
         heatMapsCarF(:,:,iFrame)=interp2(angs,dsC,heatMapsF(:,:,iFrame),angsPo2Car,dsPo2Car,'linear',0);
     end
     
-    %% 显示功率分布
+    % 显示功率分布
     hHea=figure('name','空间热度图');
     for iFrame=1:size(heatMapsCarF,3)
         figure(hHea);
         heatMap=heatMapsCarF(:,:,iFrame);
-        imagesc(xsCoor,ysCoor,heatMap);
+        heatMap=heatMap./max(max(heatMap));
+        imagesc(zsCoor,ysCoor,heatMap);
         set(gca, 'XDir','normal', 'YDir','normal');
         title(['第' num2str(ts(iFrame)) 's 的空间热度图']);
         ylabel('y(m)');
-        xlabel('x(m)');
+        xlabel('z(m)');
         pause(0.01);
     end
 end
-
