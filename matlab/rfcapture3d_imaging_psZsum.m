78ee468405397e2d5d0f09c2a596a177e0a41b95
Unknown <850576570@qq.com>
2018-03-26 16:55:06 +0800

    修复usrp_4t12r_heatmap.slx背景路径无用的问题，修复背景数据名称。添加测试数据psZsum_200kHz_2000rps_4rpf_4t12r_stand_fall.mat，测试条件：多次小范围行走，跌倒。添加测试数据标记工具rfcapture3d_psZsum_label.m，用于读取手机计时器的结果并向psZsum数据添加标签lbsChange。

--- matlab/rfcapture3d_imaging_psZsum.m
+++ matlab/rfcapture3d_imaging_psZsum.m
@@ -6,7 +6,7 @@ close all;
 doShowPsZsum=1;
 
 %% 加载/提取数据、参数
-filename='../data/psZsum_200kHz_800rps_1rpf_4t12r_ztest_stand_squat_circle.mat';
+filename='../data/psZsum_200kHz_2000rps_4rpf_4t12r_stand_fall.mat';
 load(filename)
 
 psZsum=permute(log2array(logsout,'psZsumSim'),[1,3,2]);
@@ -15,9 +15,9 @@ ts=linspace(0,size(psZsum,2)/fF,size(psZsum,2));
 
 %% 显示z轴功率分布
 if doShowPsZsum
-    psZsum=psZsum./repmat(max(psZsum),length(zsF),1);
+    psZsumAj=psZsum./repmat(max(psZsum),length(zsF),1);
     hpsZ=figure('name','目标点 z方向上各点的功率随时间变化关系图');
-    imagesc(ts,zsF,psZsum);
+    imagesc(ts,zsF,psZsumAj);
     set(gca, 'XDir','normal', 'YDir','normal');
     title('目标点 z方向上各点的功率随时间变化关系图');
     xlabel('t(s)');
---
+++ matlab/rfcapture3d_psZsum_label.m
@@ -0,0 +1,62 @@
+%% 清理
+clear;
+close all;
+
+%% 运行参数设置
+doLabel=0;
+
+%% 加载/提取数据、参数
+sFileData='../data/psZsum_200kHz_2000rps_4rpf_4t12r_stand_fall.mat';
+sFileTime='../data/timeStr_200kHz_2000rps_4rpf_4t12r_stand_fall.txt';
+load(sFileData)
+
+psZsum=permute(log2array(logsout,'psZsumSim'),[1,3,2]);
+
+ts=linspace(0,size(psZsum,2)/fF,size(psZsum,2));
+
+%% 显示z轴功率分布
+
+psZsumAj=psZsum./repmat(max(psZsum),length(zsF),1);
+hpsZ=figure('name','目标点 z方向上各点的功率随时间变化关系图');
+imagesc(ts,zsF,psZsumAj);
+set(gca, 'XDir','normal', 'YDir','normal');
+title('目标点 z方向上各点的功率随时间变化关系图');
+xlabel('t(s)');
+ylabel('z(m)');
+
+if doLabel
+    %% 读取label
+    idFileTime=fopen(sFileTime);
+    
+    % 计算时间
+    contentFileTime=textscan(idFileTime,'%f %f %f');
+    tsLabel=cumsum(contentFileTime{2}*60+contentFileTime{3});
+    tFirstLabel=input('输入z方向上功率分布图中第一个动作点的时间(s)：');
+    tsLabel=tsLabel-tsLabel(1)+tFirstLabel;
+    
+    % 计算标签
+    lb1=input('输入第一个动作点的标签：');
+    lb2=input('输入第二个动作点的标签：');
+    lbsChange=zeros(length(tsLabel),2);
+    lbsChange(1:2:end,1)=lb1;
+    lbsChange(2:2:end,1)=lb2;
+    lbsChange(:,2)=tsLabel;
+    
+    fclose(idFileTime);
+    
+    %% 保存标签和标签时间
+    save(sFileData,'lbsChange','-append');
+end
+%% 展示标签
+[~,isZMax]=max(psZsum);
+zsMax=zsF(isZMax);
+figure('name','展示标签');
+plot(ts,zsMax);
+hold on;
+isLbsChange=interp1(ts,1:length(ts),lbsChange(:,2),'nearst');
+plot(lbsChange(:,2),zsMax(isLbsChange),'o');
+text(lbsChange(:,2),double(zsMax(isLbsChange))+0.03,num2str(lbsChange(:,1)));
+hold off;
+
+
+
