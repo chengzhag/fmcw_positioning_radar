033c9b7eb667f5588d9873e5af8a13fdfa252903
Unknown <850576570@qq.com>
2018-03-26 17:34:07 +0800

    添加样本截取代码rfcapture3d_psZsum_sampleprepare.m。

---
+++ matlab/rfcapture3d_psZsum_sampleprepare.m
@@ -0,0 +1,67 @@
+%% 清理
+clear;
+close all;
+
+%% 运行参数设置
+doShowSam=1;
+
+%% 加载/提取数据、参数
+sFileData='../data/psZsum_200kHz_2000rps_4rpf_4t12r_stand_fall.mat';
+sFileTime='../data/timeStr_200kHz_2000rps_4rpf_4t12r_stand_fall.txt';
+load(sFileData)
+
+psZsum=permute(log2array(logsout,'psZsumSim'),[1,3,2]);
+
+ts=linspace(0,size(psZsum,2)/fF,size(psZsum,2));
+
+%% 显示z轴功率分布
+
+psZsumAj=psZsum./repmat(max(psZsum),length(zsF),1);
+hpsZ=figure('name','目标点 z方向上各点的功率随时间变化关系图');
+imagesc(ts,zsF,psZsumAj);
+set(gca, 'XDir','normal', 'YDir','normal');
+title('目标点 z方向上各点的功率随时间变化关系图');
+xlabel('t(s)');
+ylabel('z(m)');
+
+%% 展示标签
+[~,isZMax]=max(psZsum);
+zsMax=zsF(isZMax);
+figure('name','展示标签');
+plot(ts,zsMax);
+hold on;
+isLbsChange=interp1(ts,1:length(ts),lbsChange(:,2),'nearst');
+plot(lbsChange(:,2),zsMax(isLbsChange),'o');
+text(lbsChange(:,2),double(zsMax(isLbsChange))+0.03,num2str(lbsChange(:,1)));
+hold off;
+
+%% 选择要切割的样本标签
+lbSam=input('输入要切割的样本标签：');
+isSam=find(lbsChange(:,1)==lbSam);
+
+%% 切割样本
+isSamW=-round(tSam*fF/2):round(tSam*fF/2);
+tsSamW=isSamW/fF;
+psZsumSam=zeros(size(psZsum,1),length(isSamW),length(isSam));
+
+for i=1:length(isSam)
+    tSamCen=lbsChange(isSam(i),2);
+    iSamCen=interp1(ts,1:length(ts),tSamCen,'nearst');
+    psZsumSam(:,:,i)=psZsum(:,iSamCen+isSamW);
+end
+
+%% 显示样本
+if doShowSam
+    hSam=figure('name','显示样本');
+    for i=1:size(psZsumSam,3)
+        figure(hSam);
+        psZsumSamAj=psZsumSam(:,:,i)./repmat(max(psZsumSam(:,:,i)),length(zsF),1);
+        imagesc(tsSamW+lbsChange(isSam(i),2),zsF,psZsumSamAj);
+        set(gca, 'XDir','normal', 'YDir','normal');
+        title(['样本' num2str(i) ' 共' num2str(size(psZsumSam,3)) '个样本']);
+        xlabel('t(s)');
+        ylabel('z(m)');
+        pause(2);
+    end
+end
+
