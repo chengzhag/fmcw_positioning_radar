034c15e7a3059fc343de4c2e1ebb94902e6cb510
Unknown <850576570@qq.com>
2018-03-28 09:52:26 +0800

    为usrp_4t12r_heatmap.slx添加基于帧的跌倒检测。

---
+++ matlab/fallClassifierByFrame.m
@@ -0,0 +1,50 @@
+classdef fallClassifierByFrame < matlab.System
+    % fallClassifierByFrame
+    % 以1*19作为输入的基于帧的跌到检测
+
+    % Public, tunable properties
+    properties
+        
+    end
+
+    properties(DiscreteState)
+
+    end
+
+    % Pre-computed constants
+    properties(Access = private)
+        CompactMdl % The compacted, trained model
+    end
+
+    methods(Access = protected)
+        function setupImpl(obj)
+            % Perform one-time calculations, such as computing constants
+            % Load ECOC model from file
+            obj.CompactMdl = loadCompactModel('../data/fall_classifier_by_frame_compactModel');
+        end
+
+        function y = stepImpl(obj,u)
+            % Implement algorithm. Calculate y as a function of input u and
+            % discrete states.
+            y = predict(obj.CompactMdl,u);
+        end
+        
+        function flag = isInputSizeLockedImpl(obj,index)
+            % Return true if input size is not allowed to change while
+            % system is running
+            flag = true;
+        end
+        
+        function dataout = getOutputDataTypeImpl(~)
+            dataout = 'double';
+        end
+        
+        function sizeout = getOutputSizeImpl(~)
+            sizeout = [1 1];
+        end
+        
+        function resetImpl(obj)
+            % Initialize / reset discrete-state properties
+        end
+    end
+end
